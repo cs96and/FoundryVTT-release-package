432defbd14b10d47459a3233cc3a4726fddb6e6c
Alan Davies <alan.n.davies@gmail.com>
2024-01-28 00:13:58 +0000

    Conpstruct the API request JSON

--- publish.py
+++ publish.py
@@ -1,11 +1,46 @@
+import json
+import os
 import sys
+import urllib.request
 
 if len(sys.argv) < 3:
-	print("Usage: {os.path.filename(sys.argv[0])} <token> <url>", file=sys.stderr)
-	exit(1)
+    print(f"Usage: {os.path.filename(sys.argv[0])} <token> <url>", file=sys.stderr)
+    exit(1)
 
-print(sys.argv[1], sys.argv[2], file=sys.stderr)
+module = None
+with urllib.request.urlopen(sys.argv[2]) as f:
+    module = json.load(f)
+
+request = {
+    "id": module["id"],
+    "dry-run": True,
+    "release": {
+        "version": module["version"],
+        "manifest": sys.argv[2],
+    }
+}
+
+notes = module.get("changelog")
+if notes:
+    request["release"]["notes"] = notes
+
+moduleCompat = module.get("compatibility")
+if moduleCompat:
+    compat = {}
+    min = moduleCompat.get("minimum")
+    if min:
+        compat["minimum"] = min
+    max = moduleCompat.get("maximum")
+    if max:
+        compat["maximum"] = max
+    verified = moduleCompat.get("verified")
+    if verified:
+        moduleCompat["verified"] = verified
+    request["compatibility"] = compat
+
+requestJson = json.dumps(request)
+
+print(requestJson, file=sys.stderr)
 
 print("response-code=200")
 print("result=This is some output!")
-
