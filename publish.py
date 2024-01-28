a3cc1af5d685610440d0d6e184480bc6d44c667e
Alan Davies <alan.n.davies@gmail.com>
2024-01-28 01:55:31 +0000

    refactoring

--- publish.py
+++ publish.py
@@ -8,20 +8,20 @@ API_URL = "https://api.foundryvtt.com/_api/packages/release_version/"
 
 def main() -> int:
     if len(sys.argv) < 3:
-        print(f"Usage: {os.path.filename(sys.argv[0])} <token> <url>", file=sys.stderr)
+        printErr(f"Usage: {os.path.filename(sys.argv[0])} <token> <url>")
         return 1
 
-    module = getModule(sys.argv[2])
-    requestJson = constructRequestJson(module)
+    token, moduleUrl = sys.argv[1:3]
 
-    print(requestJson, file=sys.stderr)
+    module = getModule(moduleUrl)
+    requestJson = constructRequestJson(module, moduleUrl)
 
-    status, result = sendRequest(requestJson)
-    status = int(status)
-    result = result.decode()
+    printErr(requestJson)
 
-    print(f"Response: {status}", file=sys.stderr)
-    print(result, file=sys.stderr)
+    status, reason, result = sendRequest(requestJson, token)
+
+    printErr(f"Response: {status} {reason}")
+    printErr(result)
 
     print(f"response-code={status}")
     print("result<<EOF")
@@ -31,19 +31,25 @@ def main() -> int:
     return 0 if status == 200 else status
 
 
-def getModule(url) -> dict:
-    with urllib.request.urlopen(sys.argv[2]) as f:
+def printErr(*args, **kwargs):
+    """Print to stderr"""
+    print(*args, **kwargs, file=sys.stderr)
+
+
+def getModule(url: str) -> dict:
+    """Read the module.json from the URL"""
+    with urllib.request.urlopen(url) as f:
         return json.load(f)
 
 
-def constructRequestJson(module) -> str:
+def constructRequestJson(module: dict, url: str) -> str:
+    """Construct the JSON API request"""
     request = {
         "id": module["id"],
         "dry-run": True,
         "release": {
-            # "version": module["version"],
-            "version": "1.18.0",
-            "manifest": sys.argv[2],
+            "version": module["version"],
+            "manifest": url,
             "compatibility": {
                 "minimum": module["compatibility"]["minimum"],
                 "verified": module["compatibility"]["verified"],
@@ -60,20 +66,21 @@ def constructRequestJson(module) -> str:
     return json.dumps(request, separators=(',', ':'))
 
 
-def sendRequest(requestJson):
+def sendRequest(requestJson: str, token: str) -> (int, str, str):
+    """Send the API request to Foundry website"""
     try:
         result = urllib.request.urlopen(urllib.request.Request(
             API_URL, method="POST",
             data=requestJson.encode(),
             headers={
                 "Content-Type": "application/json",
-                "Authorization": sys.argv[1]
+                "Authorization": token
             }
         ))
     except urllib.error.HTTPError as e:
         result = e
 
-    return result.status, result.read()
+    return int(result.status), result.reason, result.read().decode()
 
 
 if __name__ == '__main__':
